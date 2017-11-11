601b68c64772104241af1ae052c05d25ae0aa399
Nick Lisauskas <nlisausk@alumni.nd.edu>
2017-11-11 13:02:35 +0000

    automatically backed up by learn

--- lib/players/human.rb
+++ lib/players/human.rb
@@ -2,8 +2,8 @@ module Players
 class Human < Player
   def move(input)
     puts "Which space would you like to occupy?"
- +      input = gets.chomp
- +      input
+      input = gets.chomp
+     input
   end
 end
 end
